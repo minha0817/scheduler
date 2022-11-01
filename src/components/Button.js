1f69018bb02c5f1ebab529b04f20df0050d10bf3
minha0817 <minhakk24@gmail.com>
2022-11-01 16:54:38 -0700

    create DayListItem

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,6 +3,7 @@ import classNames from "classnames";
 import "components/Button.scss";
 
 export default function Button(props) {
+
    const buttonClass = classNames("button", {
      "button--confirm": props.confirm,
      "button--danger": props.danger
@@ -18,3 +19,4 @@ export default function Button(props) {
      </button>
    );
  }
+
