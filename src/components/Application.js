cfe328a89aa429cd78d1c12298e97b5476645fbe
minha0817 <minhakk24@gmail.com>
2022-11-02 18:37:14 -0700

    create Appointment, Form components

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 
 import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from "./Appointment";
 
 const days = [
   {
@@ -21,10 +22,65 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  },
+  "6": {
+    id: 6,
+    time: "5pm"
+  }
+};
+
+
 
 export default function Application(props) {
 
   const [day, setDay] = useState("Monday");
+
+  const appointmentListItems = Object.values(appointments).map(appointment => {
+    return (
+      <Appointment 
+
+        key={appointment.id}
+        {...appointment}
+      
+      />
+    )
+  })
 
   return (
     <main className="layout">
@@ -40,8 +96,8 @@ export default function Application(props) {
         <nav className="sidebar__menu">
           <DayList
             days={days}
-            day={day}
-            setDay={setDay}
+            value={day}
+            onChange={setDay}
           />
         </nav>
         <img
@@ -51,7 +107,7 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity.*/}
+        {appointmentListItems}
       </section>
 
     </main>
