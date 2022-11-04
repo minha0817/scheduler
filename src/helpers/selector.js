25a77f712e0297b3730bc5210aab3bcacc6d1ca5
minha0817 <minhakk24@gmail.com>
2022-11-03 17:54:58 -0700

    Transforming data with selectors

--- src/helpers/selector.js
+++ src/helpers/selector.js
@@ -1,49 +1,24 @@
 
-const state = {
-    days: [
-      {
-        id: 1,
-        name: "Monday",
-        appointments: [1, 2, 3]
-      },
-      {
-        id: 2,
-        name: "Tuesday",
-        appointments: [4, 5]
-      }
-    ],
-    appointments: {
-      "1": { id: 1, time: "12pm", interview: null },
-      "2": { id: 2, time: "1pm", interview: null },
-      "3": {
-        id: 3,
-        time: "2pm",
-        interview: { student: "Archie Cohen", interviewer: 2 }
-      },
-      "4": { id: 4, time: "3pm", interview: null },
-      "5": {
-        id: 5,
-        time: "4pm",
-        interview: { student: "Chad Takahashi", interviewer: 2 }
-      }
-    }
-  };
+
+
+
 
 
 export function getAppointmentsForDay (state, day) {
 
-    const filteredDays = state.days.filter(user => user.name === day);
-    console.log("filteredDays:", filteredDays);
+    const filteredDay = state.days.find(user => user.name === day);
 
-    filteredDays.forEach(filteredDays => {
-        for(let key in filteredDays) {
-            console.log("filteredDays[key]", filteredDays[key])
-        }
+    if(filteredDay === undefined){
+        return [];
+    }
+
+    const assortedAppointments = filteredDay.appointments 
+
+    const result = assortedAppointments.map((id) => {
+        // console.log("result:", state.appointments[id])
+        return state.appointments[id]
     })
 
 
-
-    return filteredDays;
-
-
+    return result;
 }
