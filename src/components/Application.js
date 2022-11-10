242d16cab1f2baf5f14cf76cca7e27a1aa5173b9
minha0817 <minhakk24@gmail.com>
2022-11-10 14:32:28 -0800

    cleanup the code

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,42 +1,21 @@
-import React, { useState , useEffect} from "react";
-import axios from "axios";
-
+import React from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import Appointment from "./Appointment";
 import {getAppointmentsForDay , getInterview, getInterviewersForDay } from '../helpers/selector';
 import  useApplicationData from "hooks/useApplicationData";
 
-export default function Application(props) {
+
+export default function Application() {
 
   const { state, setDay, bookInterview, cancelInterview } = useApplicationData();
 
-                    //Application state when interview is null
-                    // {
-                    //   day: "",
-                    //   days: [],
-                    //   appointments: {
-                    //     "1": {
-                    //       id: 1,
-                    //       time: "12pm",
-                    //       interview: null
-                    //     }
-                    //   },
-                    //   interviewers: {}
-                    // }  
-                    //-------------------------------------//
-                    //How "days" look like 
-                      // const days = [
-                      //   {
-                      //     id: 1,
-                      //     name: "Monday",
-                      //     spots: 2,
-                      //   }
-                      // ];
-
   const interviewers = getInterviewersForDay(state, state.day);
+
   const appointments = getAppointmentsForDay(state, state.day).map(appointment => {
+
     return (
+
       <Appointment 
 
         key={appointment.id}
@@ -47,21 +26,22 @@ export default function Application(props) {
         bookInterview = {bookInterview}
         cancelInterview = {cancelInterview}
       />
+
     )
   })
-  // console.log(state.days, "state.days")
 
   return (
+
     <main className="layout">
 
       <section className="sidebar">
-        {/* Replace this with the sidebar elements during the "Project Setup & Familiarity" activity. */}
         <img
         className="sidebar--centered"
         src="images/logo.png"
         alt="Interview Scheduler"
         />
         <hr className="sidebar__separator sidebar--centered" />
+
         <nav className="sidebar__menu">
           <DayList
             days={state.days}
@@ -69,18 +49,22 @@ export default function Application(props) {
             setDay={setDay}
           />
         </nav>
+
         <img
           className="sidebar__lhl sidebar--centered"
           src="images/lhl.png"
           alt="Lighthouse Labs"
         />
+
       </section>
+
       <section className="schedule">
         {appointments}
         <Appointment key="last" time="5pm" />
       </section>
 
     </main>
+    
   );
 }
 
